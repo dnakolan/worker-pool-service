b54d548eb5533d42e641de4c8bbf032fe410e087
David Nakolan <david.nakolan@gmail.com>
2025-05-25 13:24:50 -0700

    Added a basic gin webserver to main.go with a health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("failed to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	slog.Info("Received terminate, graceful shutdown", "signal", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("Server Shutdown Failed", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Server exited properly")
+
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dnakolan/worker-pool-service
 
 go 1.24.3
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/google/uuid v1.6.0
+)
