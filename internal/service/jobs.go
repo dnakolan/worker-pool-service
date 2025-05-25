2e57607f6a97567984010a0ded2ecd84fe5e1814
David Nakolan <david.nakolan@gmail.com>
2025-05-25 14:23:44 -0700

    Implemented added jobs service boilerplate for /jobs endpoints

---
+++ internal/service/jobs.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+
+	"github.com/dnakolan/worker-pool-service/internal/model"
+	"github.com/dnakolan/worker-pool-service/internal/pool"
+	"github.com/google/uuid"
+)
+
+type JobsService interface {
+	CreateJobs(ctx context.Context, req *model.Job) error
+	ListJobs(ctx context.Context, filter *model.JobFilter) ([]*model.Job, error)
+	GetJobs(ctx context.Context, uid uuid.UUID) (*model.Job, error)
+}
+
+type jobsService struct {
+	pool *pool.WorkerPool
+}
+
+func NewJobsService(pool *pool.WorkerPool) *jobsService {
+	return &jobsService{pool: pool}
+}
+
+func (s *jobsService) CreateJobs(ctx context.Context, req *model.Job) error {
+	// TODO: Implement job creation
+	return nil
+}
+
+func (s *jobsService) ListJobs(ctx context.Context, filter *model.JobFilter) ([]*model.Job, error) {
+	// TODO: Implement list jobs
+	return nil, nil
+}
+
+func (s *jobsService) GetJobs(ctx context.Context, uid uuid.UUID) (*model.Job, error) {
+	// TODO: Implement get job by id
+	return nil, nil
+}
